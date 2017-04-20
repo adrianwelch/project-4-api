27ba6a88aac6524366b2057cf47e18314f41242b
adrianwelch <adriancwelch@gmail.com>
2017-04-20 16:12:37 +0100

    skyscanner

--- app/controllers/skyscanner_controller.rb
+++ app/controllers/skyscanner_controller.rb
@@ -1,15 +1,14 @@
 class SkyscannerController < ApplicationController
   skip_before_action :authenticate_user!
   def flights
-    puts params["lat"]
-    puts params["lng"]
+
     baseUrl = 'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/'
 
     tomorrow = Date.today + 1
     today = Date.today
 
     # first = HTTParty.get("#{baseUrl}#{params[:destination]}/#{tomorrow}", {
-    first = HTTParty.get("#{baseUrl}GB/GBP/en-GB/51.5074,0.1278-latlong/#{params["lat"]},#{params["lng"]}-latlong/#{today}/#{tomorrow}?", {
+    first = HTTParty.get("#{baseUrl}GB/GBP/en-GB/51.5074,0.1278-latlong/#{params["lat"]},#{params["lng"]}-latlong/#{today}/#{tomorrow}", {
       query: {
         apiKey: ENV["SKYSCANNER_API_KEY"]
       },

d760f98f12854e3fa799a2f427885e9d451195f4
adrianwelch <adriancwelch@gmail.com>
2017-04-20 15:13:03 +0100

    changes to skyscanner api

--- app/controllers/skyscanner_controller.rb
+++ app/controllers/skyscanner_controller.rb
@@ -1,12 +1,15 @@
 class SkyscannerController < ApplicationController
   skip_before_action :authenticate_user!
   def flights
-    baseUrl = 'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/UK/GBP/en-GB/LON/'
+    puts params["lat"]
+    puts params["lng"]
+    baseUrl = 'http://partners.api.skyscanner.net/apiservices/browsequotes/v1.0/'
 
     tomorrow = Date.today + 1
+    today = Date.today
 
     # first = HTTParty.get("#{baseUrl}#{params[:destination]}/#{tomorrow}", {
-    first = HTTParty.get("#{baseUrl}JFK/#{tomorrow}", {
+    first = HTTParty.get("#{baseUrl}GB/GBP/en-GB/51.5074,0.1278-latlong/#{params["lat"]},#{params["lng"]}-latlong/#{today}/#{tomorrow}?", {
       query: {
         apiKey: ENV["SKYSCANNER_API_KEY"]
       },
